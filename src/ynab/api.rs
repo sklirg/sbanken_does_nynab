b97bb803900355d9ee8365a68b2d5e4f1dca9687
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-01-18 16:21:02 +0100

    feat: Add generic http client.

--- src/ynab/api.rs
+++ src/ynab/api.rs
@@ -1,4 +1,4 @@
-use reqwest::{StatusCode};
+use reqwest::{StatusCode, Method};
 use ynab::data::{budgets_response_to_budgets};
 use ynab::helpers::{build_api_client};
 use std::io::Read;
@@ -38,4 +38,43 @@ pub fn get_ynab_budgets() {
     let budgets = budgets_response_to_budgets(body);
 
     debug!("Budgets: {:#?}", budgets);
+}
+
+fn do_api_request(url: reqwest::Url, method: Method) -> String {
+    let client = build_api_client();
+
+    let request = match method {
+        Method::GET => client
+            .get(url)
+            .send(),
+        Method::POST => client
+            .post(url)
+            .send(),
+        unhandled_method => panic!("Unhandled HTTP method: {}", unhandled_method),
+    };
+
+    let mut resp = match request {
+        Ok(resp) => resp,
+        Err(error) => {
+            error!("{}", error);
+            panic!("Response failed");
+        },
+    };
+
+    match resp.status() {
+        StatusCode::OK => trace!("HTTP OK"),
+        status => warn!("Unhandled status code: {}", status),
+    }
+
+    let mut body = String::new();
+    match resp.read_to_string(&mut body) {
+        Ok(data) => {
+            trace!("Receieved {} data", data);
+        },
+        Err(error) => {
+            error!("Failed to read response to string: {}", error);
+        }
+    }
+
+    return body;
 }
