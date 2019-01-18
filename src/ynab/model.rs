dc65fc940cb632952d2eb6aebef1a281765296c4
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-01-18 16:51:44 +0100

    feat: Add YNAB Account models.

--- src/ynab/model.rs
+++ src/ynab/model.rs
@@ -1,4 +1,9 @@
+extern crate serde;
 extern crate serde_derive;
+extern crate serde_json;
+
+use self::serde::Deserialize;
+use self::serde::de::Deserializer;
 
 pub struct YnabConfig {
     pub access_token: String,
@@ -21,4 +26,41 @@ pub struct BudgetBaseResponse {
 #[derive(Deserialize)]
 pub struct BudgetResponse {
     pub budgets: Vec<Budget>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Account {
+    pub id: String,
+    pub name: String,
+
+    #[serde(rename = "type")]
+    pub account_type: String,
+
+    pub on_budget: bool,
+    pub closed: bool,
+
+    #[serde(deserialize_with = "null_to_empty_string")]
+    pub note: String,
+    pub balance: i32,
+    pub cleared_balance: i32,
+    pub uncleared_balance: i32,
+    pub transfer_payee_id: String,
+    pub deleted: bool,
+}
+
+#[derive(Deserialize)]
+pub struct AccountResponse {
+    pub accounts: Vec<Account>,
+}
+
+#[derive(Deserialize)]
+pub struct AccountBaseResponse {
+    pub data: AccountResponse,
+}
+
+fn null_to_empty_string<'de, D>(d: D) -> Result<String, D::Error> where D: Deserializer<'de> {
+    Deserialize::deserialize(d)
+        .map(|x: Option<_>| {
+            x.unwrap_or("".to_string())
+        })
 }
